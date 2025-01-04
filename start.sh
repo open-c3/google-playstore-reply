24bd7a0f0e194a526feb183ab3a1df207a2c42ce
lijinfeng2011 <lijinfeng2011@gmail.com>
2025-01-04 20:19:04 +0800

    修改start脚本，获取端口等配置

--- start.sh
+++ start.sh
@@ -7,4 +7,15 @@ if [ "X$NAME" == "X" ];then
     exit 1
 fi
 
-docker run -it -d --name openc3-google-play-reply-$NAME -v ./code:/code -v ./conf/$NAME:/conf openc3-google-play-reply
+docker stop openc3-google-play-reply-$NAME
+docker rm openc3-google-play-reply-$NAME
+
+PORT=$(cat conf/$NAME/config.yaml |grep ^port:|awk '{print $2}' )
+docker run -it -d \
+  --name openc3-google-play-reply-$NAME \
+   -v ./code:/code \
+   -v ./conf/$NAME:/conf \
+   -v ./config.yaml:/config.yaml \
+   -p $PORT:8080 \
+   -e app_package_name=$NAME \
+   openc3-google-play-reply
