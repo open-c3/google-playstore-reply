1ad291e28d3713a8b4113d0232666423ac92469a
lijinfeng2011 <lijinfeng2011@gmail.com>
2025-01-04 16:57:16 +0800

    上报的数据添加评论人、包名等信息

--- code/main.py
+++ code/main.py
@@ -2,8 +2,24 @@
 import subprocess
 import json
 import requests
+import yaml
+import os
 
-def run_list_py():
+def load_config():
+    with open('/conf/config.yaml', 'r') as f:
+        config = yaml.safe_load(f)
+
+    # 从环境变量中获取 app_package_name 并添加到 config 字典中
+    app_package_name = os.environ.get('app_package_name')
+    if not app_package_name:
+        print("Error: app_package_name environment variable is not set.")
+        sys.exit(1)
+    config['app_package_name'] = app_package_name
+
+    return config
+
+
+def run_list_py(config):
     process = subprocess.Popen(['python', 'list.py'], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     stdout, stderr = process.communicate()
     if process.returncode != 0:
@@ -13,15 +29,16 @@ def run_list_py():
     # 处理标准输出
     for line in stdout.decode().split('\n'):
         if line.strip():
-            process_json_line(line)
+            process_json_line(line, config)
 
-def process_json_line(json_line):
+def process_json_line(json_line, config):
     # 将JSON行转换为Python对象
     data = json.loads(json_line)
     
     # 提取所需信息
 
     review_id = data['reviewId']
+    authorName = data['authorName']
 
     device_name = data['comments'][0]['userComment']['deviceMetadata']['productName']
     comment_time_seconds = data['comments'][0]['userComment']['lastModified']['seconds']
@@ -37,6 +54,7 @@ def process_json_line(json_line):
     
     # 生成新的JSON对象
     simplified_data = {
+        'author_name': authorName,
         'review_id': review_id,
         'device_name': device_name,
         'comment_time_seconds': comment_time_seconds,
@@ -48,15 +66,19 @@ def process_json_line(json_line):
         'android_os_version': android_os_version,
         'star_rating': star_rating,
         'user_comment': user_comment,
-        'developer_comment': developer_comment
+        'developer_comment': developer_comment,
+        'package_name': config['package_name'],  # 添加 package_name
+        'app_package_name': config['app_package_name'],
+        'callback': 'http://10.10.10.10:8080'
     }
     
     # 输出简化版JSON对象
     print(json.dumps(simplified_data, indent=2))
 
     # 将数据POST到指定接口
-    url = 'http://10.10.10.10/api/ci/googleplay/review/record'
+    url = config['openc3_url']
     headers = {'Content-Type': 'application/json'}
+    headers.update(config['openc3_header'])
     response = requests.post(url, headers=headers, data=json.dumps(simplified_data))
     
     if response.status_code == 200:
@@ -65,5 +87,6 @@ def process_json_line(json_line):
         print(f'Error sending data: {response.text}')
 
 if __name__ == '__main__':
-    run_list_py()
+    config = load_config()
+    run_list_py(config)
 
