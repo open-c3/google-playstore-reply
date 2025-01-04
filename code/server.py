2c603e5d21313dd74af66bfe0b58b4f7332711dd
lijinfeng2011 <lijinfeng2011@gmail.com>
2025-01-04 21:20:01 +0800

    可以配置debug参数用于调试

--- code/server.py
+++ code/server.py
@@ -4,10 +4,18 @@ import datetime
 import subprocess
 import json
 import os
+import yaml
 
 from apscheduler.schedulers.background import BackgroundScheduler
 
 app = Flask(__name__)
+
+def load_config():
+    with open('/conf/config.yaml', 'r') as f:
+        config = yaml.safe_load(f)
+    return config
+
+config = load_config()
 
 @app.route('/', methods=['POST'])
 def reply():
@@ -16,25 +24,32 @@ def reply():
         review_id = data['reviewId']
         text = data['text']
         json_data = json.dumps({'reviewId': review_id, 'text': text})
-        run_reply_script(json_data)
+        run_reply_script(json_data, config)
         return 'Data received and processed'
     else:
         return 'Invalid JSON data'
 
-def timed_job():
+def timed_job(config):
     now = datetime.datetime.now()
-    print(f'{now} /code/sync.sh ...')
-    subprocess.run(['/code/sync.sh'])
+    sync_script = '/code/sync.sh'
+    if config.get('debug', False):
+        sync_script = '/code/debug/sync.sh'
+    print(f'{now} {sync_script} ...')
+    subprocess.run([sync_script])
 
-def run_reply_script(json_data):
-    #TODO
+
+def run_reply_script(json_data, config):
     with open('/tmp/data.json', 'w') as f:
         f.write(json_data)
-    subprocess.run(['/code/reply.py'], input=json_data.encode())
+    reply_script = '/code/reply.py'
+    if config.get('debug', False):
+        reply_script = '/code/debug/reply.py'
+    subprocess.run([reply_script], input=json_data.encode())
+
 
 scheduler = BackgroundScheduler()
 
-scheduler.add_job(timed_job, 'interval', minutes=5)
+scheduler.add_job(timed_job, 'interval', minutes=5, args=[config])
 
 scheduler.start()
 
