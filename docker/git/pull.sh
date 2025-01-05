374738ef6fb6171371789a684b52ce73e8c3bb94
lijinfeng2011 <lijinfeng2011@gmail.com>
2025-01-05 23:19:29 +0800

    添加git工具

---
+++ docker/git/pull.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+SOCK=$(cat socks)
+docker run -it \
+   -v /data/google-playstore-reply:/code \
+   -v /root/.ssh:/root/.ssh \
+   -e http_proxy=socks5://$SOCK \
+   -e https_proxy=socks5://$SOCK \
+   git-client git pull
