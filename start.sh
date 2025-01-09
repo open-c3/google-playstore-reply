bd21546bb3563e379f5b2b53d78ed082e86faf67
lijinfeng2011 <lijinfeng2011@gmail.com>
2025-01-09 18:07:52 +0800

    代理可以使用socks5也可以使用http

--- start.sh
+++ start.sh
@@ -7,11 +7,12 @@ if [ "X$NAME" == "X" ];then
     exit 1
 fi
 
-docker stop openc3-google-play-reply-$NAME
-docker rm openc3-google-play-reply-$NAME
+docker stop openc3-google-play-reply-$NAME 2>/dev/null
+docker rm openc3-google-play-reply-$NAME 2>/dev/null
 
 PORT=$(cat conf/$NAME/config.yaml |grep ^port:|awk '{print $2}' )
-SOCK=$(cat conf/$NAME/config.yaml|grep ^socks5:|awk '{print $2}'|sed  "s/'//g")
+PROXY=$(cat conf/$NAME/config.yaml|grep ^proxy:|awk '{print $2}'|sed  "s/'//g")
+
 docker run -it -d \
   --name openc3-google-play-reply-$NAME \
    -v ./code:/code \
@@ -19,6 +20,6 @@ docker run -it -d \
    -v ./config.yaml:/config.yaml \
    -p $PORT:8080 \
    -e app_package_name=$NAME \
-   -e http_proxy=socks5://$SOCK \
-   -e https_proxy=socks5://$SOCK \
+   -e http_proxy=$PROXY \
+   -e https_proxy=$PROXY \
    openc3-google-play-reply
