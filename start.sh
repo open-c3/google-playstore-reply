a4accb2ab5d401c3d07a1f0302cd30fb1adcaf5f
lijinfeng2011 <lijinfeng2011@gmail.com>
2025-02-21 10:04:44 +0800

    允许不使用代理的情况

--- start.sh
+++ start.sh
@@ -13,6 +13,19 @@ docker rm openc3-google-play-reply-$NAME 2>/dev/null
 PORT=$(cat conf/$NAME/config.yaml |grep ^port:|awk '{print $2}' )
 PROXY=$(cat conf/$NAME/config.yaml|grep ^proxy:|awk '{print $2}'|sed  "s/'//g")
 
+http_proxy="-e http_proxy=$PROXY"
+https_proxy="-e https_proxy=$PROXY"
+echo "PROXY: [$PROXY]"
+
+if [ -z "$PROXY" ]; then
+    echo "Error: proxy not found in the config"
+    exit 1
+fi
+
+if [ $PROXY = "null" ]; then
+    http_proxy=""
+    https_proxy=""
+fi
 docker run -it -d \
   --name openc3-google-play-reply-$NAME \
    -v ./code:/code \
@@ -20,6 +33,6 @@ docker run -it -d \
    -v ./config.yaml:/config.yaml \
    -p $PORT:8080 \
    -e app_package_name=$NAME \
-   -e http_proxy=$PROXY \
-   -e https_proxy=$PROXY \
+   $http_proxy \
+   $https_proxy \
    openc3-google-play-reply
